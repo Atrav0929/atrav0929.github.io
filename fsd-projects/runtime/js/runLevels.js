54cd58e2f6f9357e4305e00555d3aefd8ea24c12
Atrav0929 <travel683@wearescpps.org>
2025-12-12 18:01:28 +0000

    todo 1 - 9

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,13 +18,99 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    var hitZoneSize = 25;
+    var damageFromObstacle = 10;
 
+    function createSawBlade(x, y) {
+      var sawBladeHitZone = game.createObstacle(
+        hitZoneSize,
+        damageFromObstacle
+      );
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/football.png");
+      obstacleImage.x = -25;
+      obstacleImage.y = -15;
+      sawBladeHitZone.addChild(obstacleImage);
+    }
+
+    createSawBlade(400, 250);
+    createSawBlade(800, groundY);
+    createSawBlade(1200, 250);
+    createSawBlade(1600, 300);
+    createSawBlade(2000, 300);
+
+    function createEnemy(x, y) {
+      var enemy = game.createGameItem("enemy", 25);
+      var enemyImage = draw.bitmap("img/tackle.png");
+      enemyImage.x = -25;
+      enemyImage.y = -25;
+      enemy.addChild(enemyImage);
+      enemy.x = x;
+      enemy.y = y;
+      game.addGameItem(enemy);
+
+      enemy.velocityX = -2.2;
+      enemy.rotationalVelocity = 8;
+
+      enemy.onPlayerCollision = function () {
+        game.changeIntegrity(-10);
+      };
+
+      enemy.onProjectileCollision = function () {
+        game.increaseScore(100);
+        enemy.fadeOut();
+      };
+    }
+
+    createEnemy(600, groundY - 40);
+    createEnemy(1000, groundY - 50);
+    createEnemy(1300, groundY - 60);
+    createEnemy(1700, groundY - 45);
+    createEnemy(1900, groundY - 55);
+
+    function createReward(x, y, image) {
+      var reward = game.createGameItem("reward", 25);
+      var rewardImage = draw.bitmap(image);
+      rewardImage.x = -25;
+      rewardImage.y = -25;
+      reward.addChild(rewardImage);
+      reward.x = x;
+      reward.y = y;
+      game.addGameItem(reward);
+
+      reward.velocityX = -1.5;
+
+      reward.onPlayerCollision = function () {
+        game.increaseScore(50);
+        game.changeIntegrity(10);
+        reward.fadeOut();
+      };
+
+      reward.onProjectileCollision = function () {
+        reward.fadeOut();
+      };
+    }
+
+    createReward(1800, groundY - 50, "img/Super.png");
     
 
+   
+    var endMarker = game.createGameItem("endMarker", 50);
+    var endMarkerImage = draw.bitmap("img/ball.png");
+    endMarkerImage.x = -70;
+    endMarkerImage.y = -70;
+    endMarker.addChild(endMarkerImage);
+    endMarker.x = 1600;
+    endMarker.y = groundY - 100;
+    game.addGameItem(endMarker);
+
+    endMarker.velocityX = -1.05;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+    
     function startLevel() {
       // TODO 13 goes below here
-
-
 
       //////////////////////////////////////////////
       // DO NOT EDIT CODE BELOW HERE
--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -29,7 +29,8 @@ var background = function (window) {
     // ANIMATION VARIABLES HERE //////////////////////////////////////
     //////////////////////////////////////////////////////////////////
     // TODO (several):
-
+    var field;
+    var buildings = [];
     // called at the start of game and whenever the page is resized
     // add objects for display in background. draws each image added to the background once
     function render() {
@@ -39,17 +40,17 @@ var background = function (window) {
       // this currently fills the background with an obnoxious yellow;
       // you should modify both the height and color to suit your game
       var backgroundFill = draw.bitmap("img/stands.png");
-      backgroundFill.x = 0.1
-      backgroundFill.y = 0
-      backgroundFill.scaleX = 1.25
-      backgroundFill.scaleY = 0.9
-      background.addChild(backgroundFill)
+      backgroundFill.x = 0.1;
+      backgroundFill.y = 0;
+      backgroundFill.scaleX = 1.25;
+      backgroundFill.scaleY = 0.9;
+      background.addChild(backgroundFill);
       var stand2 = draw.bitmap("img/stands.png");
-      stand2.x = 700
-      stand2.y = 0.5
-      stand2.scaleX = 2
-      stand2.scaleY = 1
-      background.addChild(stand2)
+      stand2.x = 700;
+      stand2.y = 0.5;
+      stand2.scaleX = 2;
+      stand2.scaleY = 1;
+      background.addChild(stand2);
       var groundFill = draw.rect(canvasWidth, canvasHeight - groundY, "green");
       groundFill.x = 0;
       groundFill.y = groundY;
@@ -61,10 +62,30 @@ var background = function (window) {
       Ref.scaleX = 1.0;
       Ref.scaleY = 1.0;
       background.addChild(Ref);
-      
-      // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
 
+      // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+      for (var i = 0; i < 5; ++i) {
+        var buildingHeight = 150 + Math.random() * 300;
+        var colors = ["#ff9100ff", "#ffb700ff", "#ffbf00ff", "#ff9100ff", "#ffaa00ff"];
+        var buildingColor = colors[i % colors.length];
+        var building = draw.rect(
+          75,
+          buildingHeight,
+          buildingColor,
+          buildingColor,
+          1
+        );
+        building.x = 200 * i;
+        building.y = groundY - buildingHeight;
+        background.addChild(building);
+        buildings.push(building);
+      }
       // TODO 3: Part 1 - Add a tree
+      field = draw.bitmap("img/field.png");
+      field.x = 0;
+      field.y = 0;
+      field.scaleY = 0.7;
+      background.addChild(field);
     } // end of render function - DO NOT DELETE
 
     // Perform background animation
@@ -76,7 +97,11 @@ var background = function (window) {
       var groundY = ground.y;
 
       // TODO 3: Part 2 - Move the tree!
+      field.x = field.x + 1;
 
+      if (field.x < -200) {
+        field.x = canvasWidth;
+      }
       // TODO 4: Part 2 - Parallax
     } // end of update function - DO NOT DELETE
 
